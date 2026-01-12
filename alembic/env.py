f641a1d8c805e49d220a07ec412c66e74d0309b3
phenixMaster <christophempaga@gmail.com>
2026-01-12 23:57:41 +0100

    dev:ajout alembic à requirements.txt

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import create_engine, engine_from_config
 from sqlalchemy import pool
@@ -63,11 +64,13 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+
+    url = os.getenv("DATABASE_URL", SQLALCHEMY_DATABASE_URL)
+
+    # On crée l'engine directement avec l'URL complète (incluant le mot de passe)
+    connectable = create_engine(url, poolclass=pool.NullPool)
+
+   
 
     with connectable.connect() as connection:
         context.configure(
